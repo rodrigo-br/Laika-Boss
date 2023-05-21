ea39841a864c1827a5f4b5b638b427f67bfcd155
rodrigo-br <rodrigoab123@gmail.com>
2023-05-20 21:24:04 -0300

    Enemy shooting at player

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,18 +4,41 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour, IDamageable, ICollisive
 {
+    [SerializeField] float rotateSpeed = 5f;
     HealthSystem healthSystem;
     HitEffects hitEffects;
+    Player player;
 
     void Awake()
     {
         hitEffects = GetComponent<HitEffects>();
+        player = FindObjectOfType<Player>();
     }
+
     void Start()
     {
         healthSystem = new HealthSystem(20);
 
         healthSystem.OnHealthChange += HealthSystem_OnHealthChanged;
+        if (player == null) { return; }
+        Vector2 target = player.transform.position - this.transform.position;
+        float angle = Mathf.Atan2(target.y, target.x) * Mathf.Rad2Deg;
+        this.transform.rotation = Quaternion.Euler(0, 0, angle + 270);
+        GetComponent<Shooter>().IsFiring = true;
+    }
+
+    void LateUpdate()
+    {
+        if (player == null)
+        {
+            return ;
+        }
+        Vector2 target = player.transform.position - this.transform.position;
+        float angle = Mathf.Atan2(target.y, target.x) * Mathf.Rad2Deg;
+        this.transform.rotation = Quaternion.RotateTowards(
+            this.transform.rotation,
+            Quaternion.Euler(0, 0, angle + 270),
+            rotateSpeed * Time.deltaTime);
     }
 
     void HealthSystem_OnHealthChanged(object sender, int health)
